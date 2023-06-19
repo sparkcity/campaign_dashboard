2cbc192544d2d3a848951b5260fdb5c25cea2c76
Parker <starrpe@gmail.com>
2023-06-18 23:18:19 -0400

    v1.3 debugging github pages - unterminated literal error after pyodide-worker

--- docs/app.js
+++ docs/app.js
@@ -61,10 +61,10 @@ pn.extension()
 pn.extension("plotly")
 pio.renderers.default='iframe'
 
-pc_combat_stats_df = pd.DataFrame(pd.read_excel('data/starbound_data.xlsx',
+pc_combat_stats_df = pd.DataFrame(pd.read_excel('./data/starbound_data.xlsx',
                                                    sheet_name='pc_combat_stats'))
 
-pc_rolls_df = pd.DataFrame(pd.read_excel('data/starbound_data.xlsx',
+pc_rolls_df = pd.DataFrame(pd.read_excel('./data/starbound_data.xlsx',
                                                    sheet_name='pc_rolls'))
 
 pc_color_map = {'Sparrow':'#118ab2',
