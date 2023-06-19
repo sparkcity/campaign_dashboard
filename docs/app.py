82f58022ae612a0606588add3d766ce2682adb0c
Parker <starrpe@gmail.com>
2023-06-18 22:56:27 -0400

    v1.3 edited app.py and rerun pyodide-worker

--- docs/app.py
+++ docs/app.py
@@ -1,55 +1,4 @@
-importScripts("https://cdn.jsdelivr.net/pyodide/v0.23.0/full/pyodide.js");
-
-function sendPatch(patch, buffers, msg_id) {
-  self.postMessage({
-    type: 'patch',
-    patch: patch,
-    buffers: buffers
-  })
-}
-
-async function startApplication() {
-  console.log("Loading pyodide!");
-  self.postMessage({type: 'status', msg: 'Loading pyodide'})
-  self.pyodide = await loadPyodide();
-  self.pyodide.globals.set("sendPatch", sendPatch);
-  console.log("Loaded!");
-  await self.pyodide.loadPackage("micropip");
-  const env_spec = ['markdown-it-py<3', 'https://cdn.holoviz.org/panel/1.1.0/dist/wheels/bokeh-3.1.1-py3-none-any.whl', 'https://cdn.holoviz.org/panel/1.1.0/dist/wheels/panel-1.1.0-py3-none-any.whl', 'pyodide-http==0.2.1', 'numpy', 'pandas', 'plotly']
-  for (const pkg of env_spec) {
-    let pkg_name;
-    if (pkg.endsWith('.whl')) {
-      pkg_name = pkg.split('/').slice(-1)[0].split('-')[0]
-    } else {
-      pkg_name = pkg
-    }
-    self.postMessage({type: 'status', msg: `Installing ${pkg_name}`})
-    try {
-      await self.pyodide.runPythonAsync(`
-        import micropip
-        await micropip.install('${pkg}');
-      `);
-    } catch(e) {
-      console.log(e)
-      self.postMessage({
-	type: 'status',
-	msg: `Error while installing ${pkg_name}`
-      });
-    }
-  }
-  console.log("Packages loaded!");
-  self.postMessage({type: 'status', msg: 'Executing code'})
-  const code = `
-  
-import asyncio
-
-from panel.io.pyodide import init_doc, write_doc
-
-init_doc()
-
-##############################################################################
-#                              Imports and Initializations                   #
-##############################################################################
+#Imports and Initializations
 import pandas as pd
 import numpy as np
 import panel as pn
@@ -75,9 +24,7 @@ pc_color_map = {'Sparrow':'#118ab2',
                 'Pollux':'#ffd166',
                 'Trey':'#ef476f'}
 
-##############################################################################
-#                         Party Visualization 1, 8, 9                        #
-##############################################################################
+#Party Visualization 1, 8, 9
 
 pv1_df = pc_rolls_df.groupby(['pc','context']).size().unstack(fill_value=0).reset_index()
 
@@ -99,9 +46,7 @@ pv9_fig = px.pie(pv1_df, values='Social',
                  color_discrete_map = pc_color_map,
                  title = 'Total Campaign Social Rolls Per Character')
 
-##############################################################################
-#                         Party Visualization 2                              #
-##############################################################################
+#Party Visualization 2
 
 pv2_fig = px.scatter(pc_rolls_df,
                    y='roll_total',
@@ -116,9 +61,7 @@ pv2_fig.update_layout(scattermode="group", scattergap=0.50)
 sess_min = (pc_rolls_df['session'].min()).item()
 sess_max = (pc_rolls_df['session'].max()).item()
 
-##############################################################################
-#                         Party Visualization 3, 10, 11, 12                  #
-##############################################################################
+#Party Visualization 3, 10, 11, 12
 
 pv3_gb = pc_combat_stats_df.groupby('pc').sum().reset_index()
 
@@ -150,9 +93,7 @@ pv12_fig = px.bar(pv3_gb,
                  color_discrete_map = pc_color_map,
                  title='Total Damage Mitigated Campaign Wide Per Character')
 
-##############################################################################
-#                         Party Visualization 4 - 7                          #
-##############################################################################
+# Party Visualization 4 - 7
 
 pv4_fig = px.line(pc_combat_stats_df,
                   x='session',
@@ -186,9 +127,7 @@ pv7_fig = px.line(pc_combat_stats_df,
                   markers=True,
                   title='Total Healing Done Per Character Per Session')
 
-##############################################################################
-#                         Layout Template                                    #
-##############################################################################
+# Layout Template
 
 template = pn.template.FastListTemplate(
     title='Rose\'s Thorns Roll Statistics', 
@@ -207,58 +146,3 @@ template = pn.template.FastListTemplate(
     header_background="#88d8b0",
 )
 template.servable()
-
-await write_doc()
-  `
-
-  try {
-    const [docs_json, render_items, root_ids] = await self.pyodide.runPythonAsync(code)
-    self.postMessage({
-      type: 'render',
-      docs_json: docs_json,
-      render_items: render_items,
-      root_ids: root_ids
-    })
-  } catch(e) {
-    const traceback = `${e}`
-    const tblines = traceback.split('\n')
-    self.postMessage({
-      type: 'status',
-      msg: tblines[tblines.length-2]
-    });
-    throw e
-  }
-}
-
-self.onmessage = async (event) => {
-  const msg = event.data
-  if (msg.type === 'rendered') {
-    self.pyodide.runPythonAsync(`
-    from panel.io.state import state
-    from panel.io.pyodide import _link_docs_worker
-
-    _link_docs_worker(state.curdoc, sendPatch, setter='js')
-    `)
-  } else if (msg.type === 'patch') {
-    self.pyodide.globals.set('patch', msg.patch)
-    self.pyodide.runPythonAsync(`
-    state.curdoc.apply_json_patch(patch.to_py(), setter='js')
-    `)
-    self.postMessage({type: 'idle'})
-  } else if (msg.type === 'location') {
-    self.pyodide.globals.set('location', msg.location)
-    self.pyodide.runPythonAsync(`
-    import json
-    from panel.io.state import state
-    from panel.util import edit_readonly
-    if state.location:
-        loc_data = json.loads(location)
-        with edit_readonly(state.location):
-            state.location.param.update({
-                k: v for k, v in loc_data.items() if k in state.location.param
-            })
-    `)
-  }
-}
-
-startApplication()
