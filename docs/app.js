821e167e0f65284a6652a8ad3350247e8985a740
Parker <starrpe@gmail.com>
2023-07-07 15:06:52 -0400

    v2.3 updating character highlights

--- docs/app.js
+++ docs/app.js
@@ -2,51 +2,43 @@ importScripts("https://cdn.jsdelivr.net/pyodide/v0.23.0/full/pyodide.js");
 
 function sendPatch(patch, buffers, msg_id) {
   self.postMessage({
-    type: "patch",
+    type: 'patch',
     patch: patch,
-    buffers: buffers,
-  });
+    buffers: buffers
+  })
 }
 
 async function startApplication() {
   console.log("Loading pyodide!");
-  self.postMessage({ type: "status", msg: "Loading pyodide" });
+  self.postMessage({type: 'status', msg: 'Loading pyodide'})
   self.pyodide = await loadPyodide();
   self.pyodide.globals.set("sendPatch", sendPatch);
   console.log("Loaded!");
   await self.pyodide.loadPackage("micropip");
-  const env_spec = [
-    "markdown-it-py<3",
-    "https://cdn.holoviz.org/panel/1.1.0/dist/wheels/bokeh-3.1.1-py3-none-any.whl",
-    "https://cdn.holoviz.org/panel/1.1.0/dist/wheels/panel-1.1.0-py3-none-any.whl",
-    "pyodide-http==0.2.1",
-    "numpy",
-    "pandas",
-    "plotly",
-  ];
+  const env_spec = ['markdown-it-py<3', 'https://cdn.holoviz.org/panel/1.1.0/dist/wheels/bokeh-3.1.1-py3-none-any.whl', 'https://cdn.holoviz.org/panel/1.1.0/dist/wheels/panel-1.1.0-py3-none-any.whl', 'pyodide-http==0.2.1', 'numpy', 'pandas', 'plotly']
   for (const pkg of env_spec) {
     let pkg_name;
-    if (pkg.endsWith(".whl")) {
-      pkg_name = pkg.split("/").slice(-1)[0].split("-")[0];
+    if (pkg.endsWith('.whl')) {
+      pkg_name = pkg.split('/').slice(-1)[0].split('-')[0]
     } else {
-      pkg_name = pkg;
+      pkg_name = pkg
     }
-    self.postMessage({ type: "status", msg: `Installing ${pkg_name}` });
+    self.postMessage({type: 'status', msg: `Installing ${pkg_name}`})
     try {
       await self.pyodide.runPythonAsync(`
         import micropip
         await micropip.install('${pkg}');
       `);
-    } catch (e) {
-      console.log(e);
+    } catch(e) {
+      console.log(e)
       self.postMessage({
-        type: "status",
-        msg: `Error while installing ${pkg_name}`,
+	type: 'status',
+	msg: `Error while installing ${pkg_name}`
       });
     }
   }
   console.log("Packages loaded!");
-  self.postMessage({ type: "status", msg: "Executing code" });
+  self.postMessage({type: 'status', msg: 'Executing code'})
   const code = `
   
 import asyncio
@@ -224,14 +216,21 @@ def pc_sum(nm):
 
     lvl = df["lvl"].item()
     rc = df["race"].item()
+    cls = df["class"].item()
+    arctps = df["archetypes"].item()
 
-    mx_dmg_dlt = combat_df.loc[combat_df["dmg_dealt"].idxmax()]
+    if type(arctps) == float:
+        description = f"{nm} is a level {lvl} {rc} {cls} with no Archetypes."
+    else:
+        description = f"{nm} is a level {lvl} {rc} {cls} with Archetypes in {arctps}."
+
+    mx_dmg_dlt = c_df.loc[c_df["dmg_dealt"].idxmax()]
     dmg_dlt = f"{mx_dmg_dlt['dmg_dealt']} damage dealt in Session {mx_dmg_dlt['session']}. </br> Combat encounter summary: {mx_dmg_dlt['combat_blurb']}"
-    mx_dmg_tkn = combat_df.loc[combat_df["dmg_taken"].idxmax()]
+    mx_dmg_tkn = c_df.loc[c_df["dmg_taken"].idxmax()]
     dmg_tkn = f"{mx_dmg_tkn['dmg_taken']} damage taken in Session {mx_dmg_tkn['session']}. </br> Combat encounter summary: {mx_dmg_tkn['combat_blurb']}"
-    mx_dmg_mtg = combat_df.loc[combat_df["dmg_mitigated"].idxmax()]
+    mx_dmg_mtg = c_df.loc[c_df["dmg_mitigated"].idxmax()]
     dmg_mtg = f"{mx_dmg_mtg['dmg_mitigated']} damage mitigated in Session {mx_dmg_mtg['session']}. </br> Combat encounter summary: {mx_dmg_mtg['combat_blurb']}"
-    mx_dmg_hld = combat_df.loc[combat_df["dmg_mitigated"].idxmax()]
+    mx_dmg_hld = c_df.loc[c_df["dmg_mitigated"].idxmax()]
     dmg_hld = f"{mx_dmg_hld['dmg_healed']} damage healed in Session {mx_dmg_hld['session']}. </br> Combat encounter summary: {mx_dmg_hld['combat_blurb']}"
     other = f"Total Successful Attacks: {c_df['atks_success'].sum()} </br> Total Failed Attacks: {c_df['atks_fail'].sum()} </br> Total Spells Cast: {c_df['spells_cast'].sum()} </br>Total Final Blows: {c_df['final_blows'].sum()}"
 
@@ -240,8 +239,7 @@ def pc_sum(nm):
         # Character Highlights: {nm}
         | Category      | Info |
         | ----------- | ----------- |
-        | **Level**      | {lvl}|
-        | **Race**   | {rc}|
+        | **Description**| {description}|
         | **Most Damage Dealt** | {dmg_dlt}|
         | **Most Damage Taken**| {dmg_tkn}|
         | **Most Damage Mitigated**|{dmg_mtg}|
@@ -336,45 +334,44 @@ template.servable()
 
 
 await write_doc()
-  `;
+  `
 
   try {
-    const [docs_json, render_items, root_ids] =
-      await self.pyodide.runPythonAsync(code);
+    const [docs_json, render_items, root_ids] = await self.pyodide.runPythonAsync(code)
     self.postMessage({
-      type: "render",
+      type: 'render',
       docs_json: docs_json,
       render_items: render_items,
-      root_ids: root_ids,
-    });
-  } catch (e) {
-    const traceback = `${e}`;
-    const tblines = traceback.split("\n");
+      root_ids: root_ids
+    })
+  } catch(e) {
+    const traceback = `${e}`
+    const tblines = traceback.split('\n')
     self.postMessage({
-      type: "status",
-      msg: tblines[tblines.length - 2],
+      type: 'status',
+      msg: tblines[tblines.length-2]
     });
-    throw e;
+    throw e
   }
 }
 
 self.onmessage = async (event) => {
-  const msg = event.data;
-  if (msg.type === "rendered") {
+  const msg = event.data
+  if (msg.type === 'rendered') {
     self.pyodide.runPythonAsync(`
     from panel.io.state import state
     from panel.io.pyodide import _link_docs_worker
 
     _link_docs_worker(state.curdoc, sendPatch, setter='js')
-    `);
-  } else if (msg.type === "patch") {
-    self.pyodide.globals.set("patch", msg.patch);
+    `)
+  } else if (msg.type === 'patch') {
+    self.pyodide.globals.set('patch', msg.patch)
     self.pyodide.runPythonAsync(`
     state.curdoc.apply_json_patch(patch.to_py(), setter='js')
-    `);
-    self.postMessage({ type: "idle" });
-  } else if (msg.type === "location") {
-    self.pyodide.globals.set("location", msg.location);
+    `)
+    self.postMessage({type: 'idle'})
+  } else if (msg.type === 'location') {
+    self.pyodide.globals.set('location', msg.location)
     self.pyodide.runPythonAsync(`
     import json
     from panel.io.state import state
@@ -385,8 +382,8 @@ self.onmessage = async (event) => {
             state.location.param.update({
                 k: v for k, v in loc_data.items() if k in state.location.param
             })
-    `);
+    `)
   }
-};
+}
 
-startApplication();
+startApplication()
--- src/app.py
+++ src/app.py
@@ -167,14 +167,21 @@ def pc_sum(nm):
 
     lvl = df["lvl"].item()
     rc = df["race"].item()
+    cls = df["class"].item()
+    arctps = df["archetypes"].item()
 
-    mx_dmg_dlt = combat_df.loc[combat_df["dmg_dealt"].idxmax()]
+    if type(arctps) == float:
+        description = f"{nm} is a level {lvl} {rc} {cls} with no Archetypes."
+    else:
+        description = f"{nm} is a level {lvl} {rc} {cls} with Archetypes in {arctps}."
+
+    mx_dmg_dlt = c_df.loc[c_df["dmg_dealt"].idxmax()]
     dmg_dlt = f"{mx_dmg_dlt['dmg_dealt']} damage dealt in Session {mx_dmg_dlt['session']}. </br> Combat encounter summary: {mx_dmg_dlt['combat_blurb']}"
-    mx_dmg_tkn = combat_df.loc[combat_df["dmg_taken"].idxmax()]
+    mx_dmg_tkn = c_df.loc[c_df["dmg_taken"].idxmax()]
     dmg_tkn = f"{mx_dmg_tkn['dmg_taken']} damage taken in Session {mx_dmg_tkn['session']}. </br> Combat encounter summary: {mx_dmg_tkn['combat_blurb']}"
-    mx_dmg_mtg = combat_df.loc[combat_df["dmg_mitigated"].idxmax()]
+    mx_dmg_mtg = c_df.loc[c_df["dmg_mitigated"].idxmax()]
     dmg_mtg = f"{mx_dmg_mtg['dmg_mitigated']} damage mitigated in Session {mx_dmg_mtg['session']}. </br> Combat encounter summary: {mx_dmg_mtg['combat_blurb']}"
-    mx_dmg_hld = combat_df.loc[combat_df["dmg_mitigated"].idxmax()]
+    mx_dmg_hld = c_df.loc[c_df["dmg_mitigated"].idxmax()]
     dmg_hld = f"{mx_dmg_hld['dmg_healed']} damage healed in Session {mx_dmg_hld['session']}. </br> Combat encounter summary: {mx_dmg_hld['combat_blurb']}"
     other = f"Total Successful Attacks: {c_df['atks_success'].sum()} </br> Total Failed Attacks: {c_df['atks_fail'].sum()} </br> Total Spells Cast: {c_df['spells_cast'].sum()} </br>Total Final Blows: {c_df['final_blows'].sum()}"
 
@@ -183,8 +190,7 @@ def pc_sum(nm):
         # Character Highlights: {nm}
         | Category      | Info |
         | ----------- | ----------- |
-        | **Level**      | {lvl}|
-        | **Race**   | {rc}|
+        | **Description**| {description}|
         | **Most Damage Dealt** | {dmg_dlt}|
         | **Most Damage Taken**| {dmg_tkn}|
         | **Most Damage Mitigated**|{dmg_mtg}|
