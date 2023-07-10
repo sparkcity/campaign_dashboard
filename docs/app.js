f0ed2d238dd97c47857f472660c2aed3d7fd5962
Parker <starrpe@gmail.com>
2023-07-09 22:59:55 -0400

    v2.4 replaced scatterplot with bar distribution

--- docs/app.js
+++ docs/app.js
@@ -2,43 +2,50 @@ importScripts("https://cdn.jsdelivr.net/pyodide/v0.23.0/full/pyodide.js");
 
 function sendPatch(patch, buffers, msg_id) {
   self.postMessage({
-    type: 'patch',
+    type: "patch",
     patch: patch,
-    buffers: buffers
-  })
+    buffers: buffers,
+  });
 }
 
 async function startApplication() {
   console.log("Loading pyodide!");
-  self.postMessage({type: 'status', msg: 'Loading pyodide'})
+  self.postMessage({ type: "status", msg: "Loading pyodide" });
   self.pyodide = await loadPyodide();
   self.pyodide.globals.set("sendPatch", sendPatch);
   console.log("Loaded!");
   await self.pyodide.loadPackage("micropip");
-  const env_spec = ['markdown-it-py<3', 'https://cdn.holoviz.org/panel/1.1.0/dist/wheels/bokeh-3.1.1-py3-none-any.whl', 'https://cdn.holoviz.org/panel/1.1.0/dist/wheels/panel-1.1.0-py3-none-any.whl', 'pyodide-http==0.2.1', 'pandas', 'plotly']
+  const env_spec = [
+    "markdown-it-py<3",
+    "https://cdn.holoviz.org/panel/1.1.0/dist/wheels/bokeh-3.1.1-py3-none-any.whl",
+    "https://cdn.holoviz.org/panel/1.1.0/dist/wheels/panel-1.1.0-py3-none-any.whl",
+    "pyodide-http==0.2.1",
+    "pandas",
+    "plotly",
+  ];
   for (const pkg of env_spec) {
     let pkg_name;
-    if (pkg.endsWith('.whl')) {
-      pkg_name = pkg.split('/').slice(-1)[0].split('-')[0]
+    if (pkg.endsWith(".whl")) {
+      pkg_name = pkg.split("/").slice(-1)[0].split("-")[0];
     } else {
-      pkg_name = pkg
+      pkg_name = pkg;
     }
-    self.postMessage({type: 'status', msg: `Installing ${pkg_name}`})
+    self.postMessage({ type: "status", msg: `Installing ${pkg_name}` });
     try {
       await self.pyodide.runPythonAsync(`
         import micropip
         await micropip.install('${pkg}');
       `);
-    } catch(e) {
-      console.log(e)
+    } catch (e) {
+      console.log(e);
       self.postMessage({
-	type: 'status',
-	msg: `Error while installing ${pkg_name}`
+        type: "status",
+        msg: `Error while installing ${pkg_name}`,
       });
     }
   }
   console.log("Packages loaded!");
-  self.postMessage({type: 'status', msg: 'Executing code'})
+  self.postMessage({ type: "status", msg: "Executing code" });
   const code = `
   
 import asyncio
@@ -107,8 +114,8 @@ reset_party_dist_df = party_dist_df.reset_index()
 party_dist_fig = px.bar(
     reset_party_dist_df,
     x="roll_base",
-    y="count",
-    color="count",
+    y="index",
+    color="index",
     title="Party Rolls Distribution",
 )
 party_dist_fig.update_xaxes(tickmode="linear")
@@ -330,44 +337,45 @@ template.servable()
 
 
 await write_doc()
-  `
+  `;
 
   try {
-    const [docs_json, render_items, root_ids] = await self.pyodide.runPythonAsync(code)
+    const [docs_json, render_items, root_ids] =
+      await self.pyodide.runPythonAsync(code);
     self.postMessage({
-      type: 'render',
+      type: "render",
       docs_json: docs_json,
       render_items: render_items,
-      root_ids: root_ids
-    })
-  } catch(e) {
-    const traceback = `${e}`
-    const tblines = traceback.split('\n')
-    self.postMessage({
-      type: 'status',
-      msg: tblines[tblines.length-2]
+      root_ids: root_ids,
     });
-    throw e
+  } catch (e) {
+    const traceback = `${e}`;
+    const tblines = traceback.split("\n");
+    self.postMessage({
+      type: "status",
+      msg: tblines[tblines.length - 2],
+    });
+    throw e;
   }
 }
 
 self.onmessage = async (event) => {
-  const msg = event.data
-  if (msg.type === 'rendered') {
+  const msg = event.data;
+  if (msg.type === "rendered") {
     self.pyodide.runPythonAsync(`
     from panel.io.state import state
     from panel.io.pyodide import _link_docs_worker
 
     _link_docs_worker(state.curdoc, sendPatch, setter='js')
-    `)
-  } else if (msg.type === 'patch') {
-    self.pyodide.globals.set('patch', msg.patch)
+    `);
+  } else if (msg.type === "patch") {
+    self.pyodide.globals.set("patch", msg.patch);
     self.pyodide.runPythonAsync(`
     state.curdoc.apply_json_patch(patch.to_py(), setter='js')
-    `)
-    self.postMessage({type: 'idle'})
-  } else if (msg.type === 'location') {
-    self.pyodide.globals.set('location', msg.location)
+    `);
+    self.postMessage({ type: "idle" });
+  } else if (msg.type === "location") {
+    self.pyodide.globals.set("location", msg.location);
     self.pyodide.runPythonAsync(`
     import json
     from panel.io.state import state
@@ -378,8 +386,8 @@ self.onmessage = async (event) => {
             state.location.param.update({
                 k: v for k, v in loc_data.items() if k in state.location.param
             })
-    `)
+    `);
   }
-}
+};
 
-startApplication()
+startApplication();
